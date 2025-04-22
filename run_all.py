9adb62f3152f137ce29c72d34e32078da46a2233
legendary1205 <sajjadrainbow1605@outlook.com>
2025-04-23 03:27:25 +0330

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -3,21 +3,19 @@
 import asyncio
 import threading
 from monitor_loop import monitor_loop
-from bot import main as run_bot
+from bot import run_bot
 from web.app import app
 
 def run_flask():
     app.run(host="0.0.0.0", port=9000)
 
-def run_all():
-    # Flask در thread جدا
+async def main():
     threading.Thread(target=run_flask, daemon=True).start()
 
-    # اجرای ربات به صورت sync
-    threading.Thread(target=run_bot, daemon=True).start()
-
-    # مانیتورینگ async
-    asyncio.run(monitor_loop())
+    await asyncio.gather(
+        run_bot(),          # اجرای async ربات
+        monitor_loop()      # اجرای async مانیتور
+    )
 
 if __name__ == "__main__":
-    run_all()
+    asyncio.run(main())
