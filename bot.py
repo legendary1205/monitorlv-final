8d0d7fec65cbb662a3da68750ba05fe17ff5ee87
legendary1205 <sajjadrainbow1605@outlook.com>
2025-04-23 04:46:55 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,28 +3,27 @@ from telegram.ext import (
 )
 from config import BOT_TOKEN
 from handlers.panel import panel_handler, panel_callback
-from handlers.ping import handle_ping_action, handle_ping_message
-from handlers.traffic import handle_traffic_action, handle_traffic_message
-from handlers.bandwidth import handle_bandwidth_action, handle_bandwidth_message
+from handlers.ping import handle_ping_action
+from handlers.traffic import handle_traffic_action
+from handlers.bandwidth import handle_bandwidth_action
+from handlers.shared_message_handler import handle_shared_message
 
 async def run_bot():
     app = Application.builder().token(BOT_TOKEN).build()
 
-    # دستور اصلی
+    # فرمان باز کردن پنل
     app.add_handler(CommandHandler("panel", panel_handler))
 
-    # دکمه اصلی پنل
+    # دکمه‌های پنل اصلی و بازگشت
     app.add_handler(CallbackQueryHandler(panel_callback, pattern="^monitor_|^panel$"))
 
-    # دکمه‌های زیرمجموعه برای پینگ
+    # هندل کردن دکمه‌های هر مانیتور
     app.add_handler(CallbackQueryHandler(handle_ping_action, pattern="^ping_"))
     app.add_handler(CallbackQueryHandler(handle_traffic_action, pattern="^traffic_"))
     app.add_handler(CallbackQueryHandler(handle_bandwidth_action, pattern="^bandwidth_"))
 
-    # پیام‌های متنی برای افزودن/حذف آی‌پی
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_ping_message))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_traffic_message))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_bandwidth_message))
+    # پیام‌های متنی برای افزودن/حذف IP
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_shared_message))
 
     print("🤖 Bot is running...")
 
